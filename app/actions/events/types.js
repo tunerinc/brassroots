30b6abac10ff01492e42e7531554829aecff52c1
therealaldo <agonzalez2@fullsail.edu>
2019-04-30 12:11:51 -0500

    transferred all the action types, adding flow as well

---
+++ app/actions/events/types.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @format
+ * @flow
+ */
+
+export const ADDED_TRACK: string = 'ADDED_TRACK';
+export const CLICKED_USERS_HYPERLINK: string = 'CLICKED_USERS_HYPERLINK';
+export const EDITED_PLAYLIST_SETTINGS: string = 'EDITED_PLAYLIST_SETTINGS';
+export const PLAYED_MUSIC_TRACKS: string = 'PLAYED_MUSIC_TRACKS';
+export const PLAYED_TRACK: string = 'PLAYED_TRACK';
+export const REMOVED_TRACK: string = 'REMOVED_TRACK';
+export const SEND_EVENTS_BATCH_REQUEST: string = 'SEND_EVENTS_BATCH_REQUEST';
+export const SEND_EVENTS_BATCH_SUCCESS: string = 'SEND_EVENTS_BATCH_SUCCESS';
+export const SEND_EVENTS_BATCH_FAILURE: string = 'SEND_EVENTS_BATCH_FAILURE';
+export const SENT_CHAT_MESSAGE: string = 'SENT_CHAT_MESSAGE';
+export const SKIPPED_TRACK: string = 'SKIPPED_TRACK';
+export const VIEWED_MUSIC_INFO: string = 'VIEWED_MUSIC_INFO';
