ddcd381059e6cefd2b17cedc97a6ac4c3a5f6a01
therealaldo <agonzalez2@fullsail.edu>
2019-05-02 05:43:24 -0500

    static typed state objects for routes reducer

--- app/reducers/routes.js
+++ app/reducers/routes.js
@@ -6,3 +6,26 @@
  */
 
 import updateObject from '../utils/updateObject';
+
+export type State = {
+  +scene: {},
+};
+
+/**
+ * @constant
+ * @alias routesState
+ * @type {object}
+ * 
+ * @property {object} scene The current scene based on the route stack
+ */
+export const initialState = {scene: {}};
+
+export default function reducer(
+  state: State = initialState,
+  action: {type: string} = {},
+): State {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
