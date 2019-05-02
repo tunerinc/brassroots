af7d904035bffb6d5702638cac36baae2081d704
therealaldo <agonzalez2@fullsail.edu>
2019-05-02 06:39:48 -0500

    static typed state objects for tracks reducer

--- app/reducers/tracks.js
+++ app/reducers/tracks.js
@@ -8,5 +8,126 @@
 import moment from "moment";
 import updateObject from '../utils/updateObject';
 import * as types from "../actions/tracks/types";
+import type {SpotifyError} from '../utils/spotifyAPI/types';
 
 const lastUpdated: string = moment().format("ddd, MMM D, YYYY, h:mm:ss a");
+
+export type TrackArtist = {
+  +id: string,
+  +name: string,
+};
+
+export type Track = {
+  +id: ?string,
+  +name: ?string,
+  +albumID: ?string,
+  +artists: Array<TrackArtist>,
+  +trackNumber: number,
+  +durationMS: number,
+  +totalPlays: number,
+  +userPlays: number,
+};
+
+export type State = {
+  lastUpdated: string,
+  userTracks: Array<string>,
+  tracksByID: {+[key: string]: Track},
+  totalTracks: number,
+  selectedTrack: ?string,
+  searchingTracks: boolean,
+  fetchingTracks: boolean,
+  refreshingTracks: boolean,
+  changingFavoriteTrack: boolean,
+  fetchingFavoriteTrack: boolean,
+  fetchingMostPlayed: boolean,
+  fetchingRecent: boolean,
+  addingRecent: boolean,
+  incrementingCount: boolean,
+  error: ?Error | SpotifyError,
+};
+
+/**
+ * @constant
+ * @alias singleTrackState
+ * @type {object}
+ * 
+ * @property {string}   id=null       The Spotify id of the track
+ * @property {string}   name=null     The name of the track
+ * @property {string}   albumID=null  The Spotify id of the track's album
+ * @property {object[]} artists       The track artists
+ * @property {string}   artists.id    The Spotify id of the track artist
+ * @property {string}   artists.name  The name of the track artist
+ * @property {number}   trackNumber=0 The position of the track within the album
+ * @property {number}   durationMS=0  The duration of the track in milliseconds
+ * @property {number}   totalPlays=0  The total amount of plays the track has
+ * @property {number}   userPlays=0   The amount of plays the current user has on the track
+ */
+const singleState: Track = {
+  id: null,
+  name: null,
+  albumID: null,
+  artists: [],
+  trackNumber: 0,
+  durationMS: 0,
+  totalPlays: 0,
+  userPlays: 0,
+};
+
+/**
+ * @constant
+ * @alias tracksState
+ * @type {object}
+ * 
+ * @property {string}   lastUpdated                 The date/time the tracks were last updated
+ * @property {string[]} userTracks                  The Spotify track ids saved in the current user's library
+ * @property {object}   tracksByID                  The track objects using the Spotify ids as the key
+ * @property {number}   totalTracks=0               The total amount of tracks
+ * @property {string}   selectedTrack=null          The selected track to view
+ * @property {boolean}  searchingTracks=false       Whether the current user is searching tracks
+ * @property {boolean}  fetchingTracks=false        Whether the current user is fetching tracks
+ * @property {boolean}  refreshingTracks=false      Whether the current user is refreshing tracks
+ * @property {boolean}  changingFavoriteTrack=false Whether the current user is changing their favorite track
+ * @property {boolean}  fetchingFavoriteTrack=false Whether the current user is fetching a favorite track
+ * @property {boolean}  fetchingMostPlayed=false    Whether the current user is fetching most played tracks
+ * @property {boolean}  fetchingRecent=false        Whether the current user is fetching recently played tracks
+ * @property {boolean}  addingRecent=false          Whether we are adding a recently played track for the current user
+ * @property {boolean}  incrementingCount=false     Whether the current user is incrementing the play count for a single track
+ * @property {Error}    error=null                  The error related to tracks actions
+ */
+export const initialState: State = {
+  lastUpdated,
+  userTracks: [],
+  tracksByID: {},
+  totalTracks: 0,
+  selectedTrack: null,
+  searchingTracks: false,
+  fetchingTracks: false,
+  refreshingTracks: false,
+  changingFavoriteTrack: false,
+  fetchingFavoriteTrack: false,
+  fetchingMostPlayed: false,
+  fetchingRecent: false,
+  addingRecent: false,
+  incrementingCount: false,
+  error: null,
+};
+
+function singleTrack(
+  state: Track = singleState,
+  action: {type: string},
+): Track {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default function reducer(
+  state: State = initialState,
+  action: {type: string} = {},
+): State {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
