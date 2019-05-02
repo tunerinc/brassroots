e0784f160a074759434d2200a06e2ceefed0a4a6
therealaldo <agonzalez2@fullsail.edu>
2019-05-02 04:58:28 -0500

    static typed state objects for onboarding reducer

--- app/reducers/onboarding.js
+++ app/reducers/onboarding.js
@@ -7,3 +7,38 @@
 
 import updateObject from '../utils/updateObject';
 import * as types from '../actions/onboarding/types';
+import type {SpotifyError} from '../utils/spotifyAPI/types';
+
+export type State = {
+  +onboarding: boolean,
+  +creatingUser: boolean,
+  +profileCreated: boolean,
+  +error: ?Error | SpotifyError,
+};
+
+/**
+ * @constant
+ * @alias onboardingState
+ * @type {object}
+ * 
+ * @property {boolean} onboarding=false       Whether the current user is onboarding
+ * @property {boolean} creatingUser=false     Whether the current user is creating their profile
+ * @property {boolean} profileCreated=false   Whether the current user's profile was created
+ * @property {Error}   error=null             The error related to onboarding actions
+ */
+export const initialState = {
+  onboarding: false,
+  creatingUser: false,
+  profileCreated: false,
+  error: null,
+};
+
+export default function reducer(
+  state: State = initialState,
+  action: {type: string} = {},
+): State {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
