236d84d0f1bdaa97b994f02c4d8148226d84f0c2
therealaldo <agonzalez2@fullsail.edu>
2019-05-02 06:32:53 -0500

    static typed state objects for share reducer

--- app/reducers/share.js
+++ app/reducers/share.js
@@ -10,3 +10,54 @@ import updateObject from '../utils/updateObject';
 import * as types from '../actions/share/types';
 
 const lastUpdated: string = moment().format('ddd, MMM D, YYYY, h:mm:ss a');
+
+export type State = {
+  +lastUpdated: string,
+  +sharedItem: {
+    +id: string,
+    +type: string,
+  },
+  +message: string,
+  +recipients: Array<string>,
+  +searching: boolean,
+  +sharing: boolean,
+  +error: ?Error,
+};
+
+/**
+ * @constant
+ * @alias shareState
+ * @type {object}
+ * 
+ * @property {string}   lastUpdated     The date/time the share was last updated
+ * @property {object}   sharedItem      The music item to share
+ * @property {string}   sharedItem.id   The id of the item to share
+ * @property {string}   sharedItem.type The type of item to share
+ * @property {string}   message         The message to send with the share
+ * @property {string[]} recipients      The Brassroots ids of the recipients of the share
+ * @property {boolean}  searching=false Whether the current user is searching for a user to share with
+ * @property {boolean}  sharing=false   Whether the current user is sharing media or not
+ * @property {Error}    error=null      The error related to share actions
+ */
+export const initialState: State = {
+  lastUpdated,
+  sharedItem: {
+    id: '',
+    type: '',
+  },
+  message: '',
+  recipients: [],
+  searching: false,
+  sharing: false,
+  error: null,
+};
+
+export default function reducer(
+  state: State = initialState,
+  action: {type: string} = {},
+): State {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
