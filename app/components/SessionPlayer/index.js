e68d4e16ab00974e1de566c9a891cdc4cefe8739
therealaldo <agonzalez2@fullsail.edu>
2019-06-03 22:01:49 -0500

    session player component has been moved with flow integrated

---
+++ app/components/SessionPlayer/index.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/**
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Image, TouchableOpacity} from 'react-native';
+import styles from './styles';
+
+// Icons
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+type Props = {|
+  togglePause: () => void,
+  skipNext: () => void,
+  skipPrev: () => void,
+  trackNumber: number,
+  isOwner: boolean,
+  paused: boolean,
+  queueLength: number,
+  contextLength: number,
+  image: string,
+|};
+
+type State = {||};
+
+export default class SessionPlayer extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps: Props) {
+    const {image, paused} = this.props;
+    const {image: newImage, paused: newPaused} = nextProps;
+    return image !== newImage || paused !== newPaused;
+  }
+
+  render() {
+    const {
+      togglePause,
+      skipNext,
+      skipPrev,
+      trackNumber,
+      isOwner,
+      paused,
+      queueLength,
+      contextLength,
+      image,
+    } = this.props;
+    const prevExists = isOwner && trackNumber !== 0;
+    const prevDisabled = prevExists ? false : true;
+    const prevColor = prevExists ? 'rgba(254,254,254,0.9)' : 'rgba(254,254,254,0.2)';
+    const nextExists = isOwner && (queueLength !== 0 || contextLength !== 0);
+    const nextDisabled = nextExists ? false : true;
+    const nextColor = nextExists ? 'rgba(254,254,254,0.9)' : 'rgba(254,254,254,0.2)';
+
+    return (
+      <View style={styles.player}>
+        <TouchableOpacity style={styles.left} onPress={skipPrev} disabled={prevDisabled}>
+          <Entypo name='controller-jump-to-start' style={styles.leftIcon} color={prevColor} />
+        </TouchableOpacity>
+        <View style={styles.center}>
+          <Image
+            style={styles.image}
+            resizeMode='cover'
+            resizeMethod='scale'
+            source={{uri: image}}
+          />
+          <TouchableOpacity style={styles.playPause} onPress={togglePause}>
+            {paused &&
+              <FontAwesome
+                name='pause'
+                size={75}
+                color='rgba(254,254,254,0.9)'
+                style={styles.centerIcon}
+              />
+            }
+            {!paused &&
+              <Entypo
+                name='controller-play'
+                size={125}
+                color='rgba(254,254,254,0.9)'
+                style={styles.centerIcon}
+              />
+            }
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.right} onPress={skipNext} disabled={nextDisabled}>
+          <Entypo name='controller-next' style={styles.rightIcon} color={nextColor} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
