86675fdcfb4a4805d06f79cb116e01ec6baf1ba6
therealaldo <agonzalez2@fullsail.edu>
2019-06-03 20:05:21 -0500

    mini player component has been moved with flow integrated

---
+++ app/components/MiniPlayer/index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/**
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import Dimensions from 'Dimensions';
+import {Text, View, Image, TouchableOpacity, Animated} from 'react-native';
+import styles from './styles';
+
+// Icons
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+type Props = {|
+  openPlayer: () => void,
+  navToProfile: () => void,
+  togglePause: () => void,
+  progress: number,
+  durationMS: number,
+  profileImage: string,
+  name: string,
+  artists: string,
+  displayName: string,
+  paused: boolean,
+|};
+
+type State = {||};
+
+export const screenWidth: number = Dimensions.get('window').width;
+
+export default class MiniPlayer extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+  }
+  
+  render() {
+    const {
+      openPlayer,
+      navToProfile,
+      togglePause,
+      progress,
+      durationMS,
+      profileImage,
+      name,
+      artists,
+      displayName,
+      paused,
+    } = this.props;
+
+    return (
+      <Animated.View style={styles.player}>
+        <Animated.View
+          style={[
+            styles.playerProgress,
+            {width: screenWidth * (progress / durationMS)}
+          ]}
+        ></Animated.View>
+        <TouchableOpacity style={styles.playerButton} onPress={openPlayer}>
+          <TouchableOpacity style={styles.playerImageButton} onPress={navToProfile}>
+            <Image style={styles.playerImage} source={{uri: profileImage}} />
+          </TouchableOpacity>
+          <View style={styles.playerInfo}>
+            <Text numberOfLines={1} style={styles.playerTrack}>
+              <Text style={styles.playerTrackName}>
+                {name}
+              </Text>
+              <Text style={styles.playerTrackSeparator}> • </Text>
+              {artists}
+            </Text>
+            <Text numberOfLines={1} style={styles.playerSessionOwner}>
+              {displayName}
+            </Text>
+          </View>
+          <TouchableOpacity style={styles.playerAction} onPress={togglePause}>
+            {paused && <Entypo name='controller-play' color='#fefefe' style={styles.playIcon} />}
+            {!paused && <Ionicons name='md-pause' color='#fefefe' style={styles.pauseIcon} />}
+          </TouchableOpacity>
+        </TouchableOpacity>
+      </Animated.View>
+    );
+  }
+}
