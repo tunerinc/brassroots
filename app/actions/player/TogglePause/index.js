4f361a1fd874a1584a9ad5033e63e1ad54e2503d
therealaldo <agonzalez2@fullsail.edu>
2019-07-05 14:12:51 -0500

    toggle pause is working as expected

--- app/actions/player/TogglePause/index.js
+++ app/actions/player/TogglePause/index.js
@@ -11,6 +11,7 @@
 
 import moment from 'moment';
 import Spotify from 'rn-spotify-sdk';
+import updateObject from '../../../utils/updateObject';
 import * as actions from './actions';
 import {type ThunkAction} from '../../../reducers/player';
 import {
@@ -33,17 +34,17 @@ type Session = {|
  * 
  * @author Aldo Gonzalez <aldo@tunerinc.com>
  *
- * @param    {string}  userID             The user id of the person pausing the session
- * @param    {string}  ownerID            The user id of the session owner
- * @param    {object}  session            The session object to pause the track in for the current user
- * @param    {string}  session.id         The id of the session to pause
- * @param    {string}  session.current    The currently playing track in the session
- * @param    {number}  [session.progress] The current progress of the session the user is pausing
- * @param    {boolean} paused             The new pause status for the current user in the now playing session
+ * @param    {string}  userID           The user id of the person pausing the session
+ * @param    {string}  ownerID          The user id of the session owner
+ * @param    {object}  session          The session object to pause the track in for the current user
+ * @param    {string}  session.id       The id of the session to pause
+ * @param    {string}  session.current  The currently playing track in the session
+ * @param    {number}  session.progress The current progress of the session the user is pausing
+ * @param    {boolean} paused           The new pause status for the current user in the now playing session
  *
  * @return   {Promise}
- * @resolves {object}                     The now playing session with the new pause status for the current user
- * @reject   {Error}                      The error which caused the toggle pause track failure
+ * @resolves {object}                   The now playing session with the new pause status for the current user
+ * @reject   {Error}                    The error which caused the toggle pause track failure
  */
 export function togglePause(
   userID: string,
@@ -59,56 +60,37 @@ export function togglePause(
     const sessionUserRef: FirestoreDoc = sessionRef.collection('users').doc(userID);
 
     let batch: FirestoreBatch = firestore.batch();
-    let newPosition: number = 0;
     let playbackState: {position?: number} = {};
 
     try {
       if (!paused) {
-        playbackState = await Spotify.getPlaybackStateAsync();
-        console.log(playbackState)
-
-        if (!playbackState || Object.keys(playbackState).length === 0) {
-          throw new Error('Unable to retrieve playback state');
-        }
-      }
-
-      if (
-        Object.keys(playbackState).length !== 0
-        && !paused
-        && session.progress
-        && playbackState.position !== session.progress
-      ) {
-        newPosition = session.progress;
-        console.log('played')
-        await Spotify.setPlaying(true);
+        await Spotify.playURI(`spotify:track:${session.current}`, 0, session.progress / 1000);
       } else {
-        console.log('paused')
         await Spotify.setPlaying(false);
       }
 
       if (paused) {
         playbackState = await Spotify.getPlaybackStateAsync();
-        console.log(playbackState)
 
-        if (!playbackState || Object.keys(playbackState).length === 0) {
+        if (!playbackState || typeof playbackState.position !== 'number') {
           throw new Error('Unable to retrieve playback state');
         }
-      }
 
-      if (newPosition === 0 && playbackState.position) {
-        newPosition = playbackState.position * 1000;
+        session = updateObject(session, {progress: playbackState.position * 1000});
       }
 
       const timeLastPlayed: string = moment().format('ddd, MMM D, YYYY, h:mm:ss a');
 
-      batch.update(sessionUserRef, {paused, currentProgressMS: newPosition});
+      if (typeof playbackState.position === 'number') {
+        batch.update(sessionUserRef, {paused, progress: session.progress});
+      }
 
-      if (ownerID === userID) {
-        batch.update(sessionRef, {timeLastPlayed, paused, currentProgressMS: newPosition});
+      if (ownerID === userID && typeof playbackState.position === 'number') {
+        batch.update(sessionRef, {timeLastPlayed, paused, progress: session.progress});
       }
 
       await batch.commit();
-      dispatch(actions.togglePauseSuccess(paused, newPosition + 1000));
+      dispatch(actions.togglePauseSuccess(paused, session.progress + 1000));
     } catch (err) {
       dispatch(actions.togglePauseFailure(err));
     };
