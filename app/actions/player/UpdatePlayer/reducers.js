929b0b2187a8d7664b9adce81effc4f2a57e8a7c
therealaldo <agonzalez2@fullsail.edu>
2019-07-15 22:12:03 -0500

    wrote reducer case function

---
+++ app/actions/player/UpdatePlayer/reducers.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @format
+ * @flow
+ */
+
+/**
+ * @module UpdatePlayerReducers
+ */
+
+import updateObject from '../../../utils/updateObject';
+import {
+  type Action,
+  type State,
+} from '../../../reducers/player';
+
+/**
+ * Updates the player state with new information
+ * 
+ * @function updatePlayer
+ * 
+ * @author Aldo Gonzalez <aldo@tunerinc.com>
+ * 
+ * @param {object}   state 
+ * @param {object}   action 
+ * @param {string}   action.type
+ * @param {object}   action.updates
+ * @param {boolean}  [action.updates.attemptingToPlay] The new status for whether the current user is attempting to play a track
+ * @param {string}   [action.updates.prevTrackID]       The Spotify id for the new previous track
+ * @param {string}   [action.updates.prevQueueID]       The queue id for the new previous track
+ * @param {string}   [action.updates.currentTrackID]    The Spotify id for the new current track
+ * @param {string}   [action.updates.currentQueueID]    The queue id for the new current track
+ * @param {string}   [action.updates.nextTrackID]       The Spotify id for the new next track
+ * @param {string}   [action.updates.nextQueueID]       The queue id for the new next track
+ * 
+ * @returns {object}                                    The state with new updates added
+ */
+export function updatePlayer(
+  state: State,
+  action: Action,
+): State {
+  const {updates} = action;
+  return updateObject(state, updates);
+}
