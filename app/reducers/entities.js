c8045774698f41be580e3baeafeccddd50357e8c
therealaldo <agonzalez2@fullsail.edu>
2019-08-05 22:57:05 -0500

    added initial state with jsdocs

--- app/reducers/entities.js
+++ app/reducers/entities.js
@@ -98,3 +98,121 @@ type State = {|
   },
 |};
 
+
+
+export type {
+  GetState,
+  PromiseAction,
+  ThunkAction,
+  Dispatch,
+  Action,
+  State,
+};
+
+/**
+ * @constant
+ * @alias entitiesState
+ * @type {object}
+ * 
+ * @property {object}   albums
+ * @property {string[]} albums.allIDs
+ * @property {number}   albums.total
+ * @property {object}   albums.byID
+ * @property {object}   artists
+ * @property {string[]} artists.allIDs
+ * @property {number}   artists.total
+ * @property {object}   artists.byID
+ * @property {object}   conversations
+ * @property {string[]} conversations.allIDs
+ * @property {number}   conversations.total
+ * @property {object}   conversations.byID
+ * @property {object}   groups
+ * @property {string[]} groups.allIDs
+ * @property {number}   groups.total
+ * @property {object}   groups.byID
+ * @property {object}   messages
+ * @property {number}   messages.total
+ * @property {string[]} messages.allIDs
+ * @property {object}   messages.byID
+ * @property {object}   playlists
+ * @property {string[]} playlists.allIDs
+ * @property {number}   playlists.total
+ * @property {object}   playlists.byID
+ * @property {object}   playlistTracks
+ * @property {string[]} playlistTracks.allIDs
+ * @property {number}   playlistTracks.total
+ * @property {object}   playlistTracks.byID
+ * @property {object}   queueTracks
+ * @property {string[]} queueTracks.allIDs
+ * @property {number}   queueTracks.total
+ * @property {object}   queueTracks.byID
+ * @property {object}   sessions
+ * @property {string[]} sessions.allIDs
+ * @property {number}   sessions.total
+ * @property {object}   sessions.byID
+ * @property {object}   tracks
+ * @property {string[]} tracks.allIDs
+ * @property {number}   tracks.total
+ * @property {object}   tracks.byID
+ * @property {object}   users
+ * @property {string[]} users.allIDs
+ * @property {number}   users.total
+ * @property {object}   users.byID
+ */
+export const initialState: State = {
+  albums: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  artists: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  conversations: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  groups: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  messages: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  playlists: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  playlistTracks: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  queueTracks: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  sessions: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  tracks: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+  users: {
+    allIDs: [],
+    total: 0,
+    byID: {},
+  },
+};
