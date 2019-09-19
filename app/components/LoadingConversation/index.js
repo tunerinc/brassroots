aac565cd095761e3a68baa43fdea735c4da41f4c
therealaldo <agonzalez2@fullsail.edu>
2019-09-18 19:17:59 -0500

    updated loading conversation component to have new placeholder

--- app/components/LoadingConversation/index.js
+++ app/components/LoadingConversation/index.js
@@ -7,54 +7,41 @@
 
 import React from 'react';
 import {View} from 'react-native';
-import Placeholder from 'rn-placeholder';
+import {Placeholder, PlaceholderMedia, PlaceholderLine, Fade} from 'rn-placeholder';
 import styles from './styles';
 
 type Props = {||};
 type State = {||};
 
 export default class LoadingConversation extends React.PureComponent<Props, State> {
+  renderImage = () => <PlaceholderMedia isRound={true} style={styles.image} />;
+
   render() {
+    const topStyles = [styles.text, styles.topText];
+
     return (
       <View style={styles.container}>
-        <View style={styles.image}>
-          <Placeholder.Media
-            animate='fade'
-            size={60}
-            hasRadius={true}
-            color='#888'
-          />
+        <View style={styles.imageWrap}>
+          <Placeholder Animate={Fade} Left={this.renderImage} />
         </View>
         <View style={styles.info}>
           <View style={styles.header}>
-            <View style={styles.member}>
-              <Placeholder.Line
-                animate='fade'
-                textSize={20}
-                lineSpacing={4}
-                color='#888'
-                width='100%'
-              />
+            <View style={styles.memberWrap}>
+              <Placeholder Animate={Fade}>
+                <PlaceholderLine width={100} style={styles.text} />
+              </Placeholder>
             </View>
-            <View style={styles.timestamp}>
-              <Placeholder.Line
-                animate='fade'
-                textSize={16}
-                lineSpacing={3.2}
-                color='#888'
-                width='100%'
-              />
+            <View style={styles.timestampWrap}>
+              <Placeholder Animate={Fade}>
+                <PlaceholderLine width={100} style={styles.timestamp} />
+              </Placeholder>
             </View>
           </View>
           <View style={styles.content}>
-            <Placeholder.Paragraph
-              animate='fade'
-              lineNumber={2}
-              textSize={20}
-              lineSpacing={4}
-              color='#888'
-              width='100%'
-            />
+            <Placeholder Animate={Fade}>
+              <PlaceholderLine width={100} style={topStyles} />
+              <PlaceholderLine width={100} style={styles.text} />
+            </Placeholder>
           </View>
         </View>
       </View>
--- app/components/LoadingConversation/styles.js
+++ app/components/LoadingConversation/styles.js
@@ -11,12 +11,16 @@ import {type ViewStyleProp} from 'react-native/Libraries/StyleSheet/StyleSheet';
 
 interface Styles {
   container: ViewStyleProp,
+  imageWrap: ViewStyleProp,
   image: ViewStyleProp,
   info: ViewStyleProp,
   header: ViewStyleProp,
-  member: ViewStyleProp,
+  memberWrap: ViewStyleProp,
+  timestampWrap: ViewStyleProp,
   timestamp: ViewStyleProp,
   content: ViewStyleProp,
+  text: ViewStyleProp,
+  topText: ViewStyleProp,
 };
 
 const styles: Styles = StyleSheet.create({
@@ -29,9 +33,14 @@ const styles: Styles = StyleSheet.create({
     borderBottomWidth: 1,
     opacity: 0.3,
   },
-  image: {
+  imageWrap: {
     flex: 1,
     alignSelf: 'flex-start',
+  },
+  image: {
+    width: 55,
+    height: 55,
+    backgroundColor: '#888',
   },
   info: {
     flex: 4,
@@ -43,15 +52,26 @@ const styles: Styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
   },
-  member: {
+  memberWrap: {
     flex: 3,
     paddingRight: 20,
   },
-  timestamp: {
+  timestampWrap: {
     flex: 1,
+  },
+  timestamp: {
+    height: 16,
+    backgroundColor: '#888',
   },
   content: {
     flex: 2,
+  },
+  text: {
+    height: 20,
+    backgroundColor: '#888',
+  },
+  topText: {
+    marginBottom: 6.2,
   },
 });
 
