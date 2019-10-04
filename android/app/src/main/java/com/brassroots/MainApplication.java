2aa840bd05c68194909dedf72318ff695d719f04
therealaldo <agonzalez2@fullsail.edu>
2019-10-03 21:35:39 -0500

    archived 0.1.1

--- android/app/src/main/java/com/brassroots/MainApplication.java
+++ android/app/src/main/java/com/brassroots/MainApplication.java
@@ -7,8 +7,6 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.lufinkey.react.eventemitter.RNEventEmitterPackage;
-import com.spotify.sdk.android.authentication.RNSpotifyPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
