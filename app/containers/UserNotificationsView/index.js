35e9507c3e5c3e6c5a2d3c1db9fa66af0bf32566
Aldo Gonzalez <agonzalez2@fullsail.edu>
2019-10-07 22:43:53 -0500

    Add user notifications view (#32)

--- app/containers/UserNotificationsView/index.js
+++ app/containers/UserNotificationsView/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React from 'react';
-import {Text, View, ScrollView, Animated, Easing} from 'react-native';
+import PropTypes from 'prop-types';
+import {Text, View, TouchableOpacity, ScrollView, Animated, Easing} from 'react-native';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {Actions} from 'react-native-router-flux';
@@ -10,15 +11,109 @@ import styles from './styles';
 // Icons
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// Settings Action Creators
+import {saveSettings} from '../../actions/settings/SaveSettings';
+
 class UserNotificationsView extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       shadowOpacity: new Animated.Value(0),
+      tempSession: '',
+      tempChat: '',
+      tempMessage: true,
+      tempGroup: '',
+      tempNearby: '',
+      tempChange: true,
+      tempJoin: true,
+      tempLike: true,
+      tempFollow: true,
     };
 
+    this.setSetting = this.setSetting.bind(this);
+    this.handleSaveSettings = this.handleSaveSettings.bind(this);
     this.onScroll = this.onScroll.bind(this);
+  }
+
+  componentDidMount() {
+    const {settings: {notify}} = this.props;
+
+    this.setState({
+      tempSession: notify.session,
+      tempChat: notify.chat,
+      tempMessage: notify.message,
+      tempGroup: notify.groupMessage,
+      tempNearby: notify.nearbySession,
+      tempChange: notify.playlistChange,
+      tempJoin: notify.playlistJoin,
+      tempLike: notify.likedTrack,
+      tempFollow: notify.newFollower,
+    });
+  }
+
+  componentWillUnmount() {
+    const {
+      tempSession,
+      tempChat,
+      tempMessage,
+      tempGroup,
+      tempNearby,
+      tempChange,
+      tempJoin,
+      tempLike,
+      tempFollow,
+    } = this.state;
+    const {settings: {notify}} = this.props;
+
+    if (
+      tempSession !== notify.session
+      || tempChat !== notify.chat
+      || tempMessage !== notify.message
+      || tempGroup !== notify.groupMessage
+      || tempNearby !== notify.nearbySession
+      || tempChange !== notify.playlistChange
+      || tempJoin !== notify.playlistJoin
+      || tempLike !== notify.likedTrack
+      || tempFollow !== notify.newFollower
+    ) {
+      this.handleSaveSettings();
+    }
+  }
+
+  setSetting = updates => () => this.setState({...updates});
+
+  handleSaveSettings() {
+    const {
+      tempSession,
+      tempChat,
+      tempMessage,
+      tempGroup,
+      tempNearby,
+      tempChange,
+      tempJoin,
+      tempLike,
+      tempFollow,
+    } = this.state;
+
+    const {saveSettings, users: {currentUserID}} = this.props;
+
+    saveSettings(
+      {
+        id: currentUserID,
+        notify: {
+          session: tempSession,
+          chat: tempChat,
+          message: tempMessage,
+          groupMessage: tempGroup,
+          nearbySession: tempNearby,
+          playlistChange: tempChange,
+          playlistJoin: tempJoin,
+          likedTrack: tempLike,
+          newFollower: tempFollow,
+        },
+      },
+    );
   }
 
   onScroll({nativeEvent: {contentOffset: {y}}}) {
@@ -42,7 +137,29 @@ class UserNotificationsView extends React.Component {
   }
 
   render() {
-    const {shadowOpacity} = this.state;
+    const {
+      shadowOpacity,
+      tempSession,
+      tempChat,
+      tempMessage,
+      tempGroup,
+      tempNearby,
+      tempChange,
+      tempJoin,
+      tempLike,
+      tempFollow,
+    } = this.state;
+    const opacity = (setting, option) => {
+      if (typeof setting === 'boolean') {
+        if (option === 'off' && !setting) return 1;
+        if (option === 'off' && setting) return 0;
+        if (option === 'on' && setting) return 1;
+        if (option === 'on' && !setting) return 0;
+      } else {
+        if (setting === option) return 1;
+        if (setting !== option) return 0;
+      }
+    };
 
     return (
       <View style={styles.container}>
@@ -54,18 +171,387 @@ class UserNotificationsView extends React.Component {
           </View>
         </Animated.View>
         <ScrollView style={styles.wrap} onScroll={this.onScroll} scrollEventThrottle={16}>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>LIVE SESSIONS</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempSession: 'off'})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempSession, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempSession: 'following'})}
+              >
+                <Text style={styles.sectionOptionText}>following</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempSession, 'following')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>LIVE SESSION CHAT</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempChat: 'off'})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempChat, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempChat: 'mentions'})}
+              >
+                <Text style={styles.sectionOptionText}>mentions</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempChat, 'mentions')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempChat: 'all'})}
+              >
+                <Text style={styles.sectionOptionText}>all chat messages</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempChat, 'all')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>DIRECT MESSAGES</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempMessage: false})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempMessage, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempMessage: true})}
+              >
+                <Text style={styles.sectionOptionText}>on</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempMessage, 'on')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>GROUP DIRECT MESSAGES</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempGroup: 'off'})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempGroup, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempGroup: 'mentions'})}
+              >
+                <Text style={styles.sectionOptionText}>mentions</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempGroup, 'mentions')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempGroup: 'all'})}
+              >
+                <Text style={styles.sectionOptionText}>all messages</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempGroup, 'all')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>NEARBY LIVE SESSIONS</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempNearby: 'off'})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempNearby, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempNearby: 'following'})}
+              >
+                <Text style={styles.sectionOptionText}>following</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempNearby, 'following')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempNearby: 'all'})}
+              >
+                <Text style={styles.sectionOptionText}>all nearby sessions</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempNearby, 'all')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>PLAYLIST CHANGES</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempChange: false})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempChange, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempChange: true})}
+              >
+                <Text style={styles.sectionOptionText}>on</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempChange, 'on')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>PLAYLIST JOINS</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempJoin: false})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempJoin, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempJoin: true})}
+              >
+                <Text style={styles.sectionOptionText}>on</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempJoin, 'on')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>LIKED SONGS</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempLike: false})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempLike, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempLike: true})}
+              >
+                <Text style={styles.sectionOptionText}>on</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempLike, 'on')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionHeaderText}>NEW FOLLOWERS</Text>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempFollow: false})}
+              >
+                <Text style={styles.sectionOptionText}>off</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempFollow, 'off')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.sectionOption}>
+              <TouchableOpacity
+                style={styles.sectionOptionWrap}
+                onPress={this.setSetting({tempFollow: true})}
+              >
+                <Text style={styles.sectionOptionText}>on</Text>
+                <Ionicons
+                  name='md-checkmark'                  
+                  style={[
+                    styles.optionCheck,
+                    {opacity: opacity(tempFollow, 'on')},
+                  ]}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
         </ScrollView>
       </View>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {};
+UserNotificationsView.propTypes = {
+  saveSettings: PropTypes.func.isRequired,
+  settings: PropTypes.object.isRequired,
+  users: PropTypes.object.isRequired,
+};
+
+function mapStateToProps({settings, users}) {
+  return {
+    settings,
+    users,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({}, dispatch);
+  return bindActionCreators({saveSettings}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserNotificationsView);
