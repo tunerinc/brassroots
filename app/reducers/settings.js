1219b41c77bba590e64004ad480474e4a6c26c47
therealaldo <agonzalez2@fullsail.edu>
2019-10-09 17:38:59 -0500

    bumped version to 0.1.6

--- app/reducers/settings.js
+++ app/reducers/settings.js
@@ -149,7 +149,7 @@ export type {
  */
 export const initialState: State = {
   lastUpdated,
-  version: '0.1.5',
+  version: '0.1.6',
   initializing: false,
   loggingIn: false,
   loggedIn: false,
