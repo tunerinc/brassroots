29e43fa39d0b3bda3a5c9ecf79011eeefb757603
therealaldo <agonzalez2@fullsail.edu>
2019-10-31 19:28:15 -0500

    archiving 0.3.2

--- app/reducers/settings.js
+++ app/reducers/settings.js
@@ -149,7 +149,7 @@ export type {
  */
 export const initialState: State = {
   lastUpdated,
-  version: '0.3.1',
+  version: '0.3.2',
   initializing: false,
   loggingIn: false,
   loggedIn: false,
