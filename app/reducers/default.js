322b58c856a11235cd0a0131cfca7cc7f89ad31d
Obinna Okoro <obinna.okoroeugene@gmail.com>
2020-06-11 21:28:37 +0100

    add socket io client with base url

---
+++ app/reducers/default.js
@@ -0,0 +1,11 @@
+import SocketIOClient from 'socket.io-client';
+const host = {
+    development: "http://localhost:8082",
+    production: "https://brassroots-api.herokuapp.com",
+}
+const BASE_URL = host.development,
+    socket = SocketIOClient(`${BASE_URL}`);
+
+export default {
+    socket,
+}
