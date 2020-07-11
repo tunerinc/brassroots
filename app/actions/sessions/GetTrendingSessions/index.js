6328e211a7304161ab61fc415f2bd5322c766a21
Obinna Okoro <obinna.okoroeugene@gmail.com>
2020-07-11 19:05:41 +0100

    implement real time listeners for trending

--- app/actions/sessions/GetTrendingSessions/index.js
+++ app/actions/sessions/GetTrendingSessions/index.js
@@ -16,8 +16,8 @@ import getUserLocation from '../../../utils/getUserLocation';
 import calculateDistance from '../../../utils/calculateDistance';
 import updateObject from '../../../utils/updateObject';
 import * as actions from './actions';
-import {addEntities} from '../../entities/AddEntities';
-import {updateSessions} from '../UpdateSessions';
+import { addEntities } from '../../entities/AddEntities';
+import { updateSessions } from '../UpdateSessions';
 import {
   type ThunkAction,
   type Session,
@@ -28,11 +28,11 @@ import {
   type FirestoreDocs,
   type FirestoreDoc,
 } from '../../../utils/firebaseTypes';
-import {type Playlist} from '../../../reducers/playlists';
-import {type User} from '../../../reducers/users';
-import {type Album} from '../../../reducers/albums';
-import {type Artist} from '../../../reducers/artists';
-import {type Track} from '../../../reducers/tracks';
+import { type Playlist } from '../../../reducers/playlists';
+import { type User } from '../../../reducers/users';
+import { type Album } from '../../../reducers/albums';
+import { type Artist } from '../../../reducers/artists';
+import { type Track } from '../../../reducers/tracks';
 
 type Coords = {
   coords?: {
@@ -58,7 +58,7 @@ type Coords = {
 export function getTrendingSessions(
   userID: string,
 ): ThunkAction {
-  return async (dispatch, _, {getFirestore}) => {
+  return async (dispatch, _, { getFirestore }) => {
     dispatch(actions.request());
 
     const firestore: FirestoreInstance = getFirestore();
@@ -66,7 +66,7 @@ export function getTrendingSessions(
 
     let playlists = {};
     let users = {};
-    let pos: Coords = {coords: {}};
+    let pos: Coords = { coords: {} };
 
     try {
       const permission: string = await Permissions.request('location', { type: 'whenInUse' });
@@ -74,91 +74,108 @@ export function getTrendingSessions(
       if (permission === 'authorized') {
         pos = await getUserLocation();
       } else if (permission === 'undetermined') {
-        await Permissions.request('location', {type: 'whenInUse'});
+        await Permissions.request('location', { type: 'whenInUse' });
         pos = await getUserLocation();
       }
 
       if (pos.coords && Object.keys(pos.coords).length !== 0) {
-        users = updateObject(users, {[userID]: {id: userID, coords: pos.coords}});
+        users = updateObject(users, { [userID]: { id: userID, coords: pos.coords } });
       }
 
-      const trendingSessions: FirestoreDocs = await sessionsRef.where('live', '==', true)
+      var unsubscribe = sessionsRef.where('live', '==', true)
         .orderBy('totals.listeners', 'desc')
         .limit(15)
-        .get();
+        .onSnapshot(async (snapshot) => {
+          const trendingSessions: FirestoreDocs = snapshot;
+          await doSomething(trendingSessions, true)
+        })
 
-      if (trendingSessions.empty) {
-        dispatch(updateSessions({explore: {trendingIDs: [], trendingCanPaginate: true}}));
-        dispatch(actions.success());
-      } else {
-        const trendingIDs: Array<string> = trendingSessions.docs.map(doc => doc.data().id);
-        const trendingCanPaginate: boolean = trendingIDs.length === 15;
-        const tracksToFetch: Array<string> = trendingSessions.docs.map(doc => doc.data().currentTrackID);
-        const trackRes = await Spotify.getTracks(tracksToFetch, {});
-        const music = addMusicItems(trackRes.tracks);
-        const sessions = trendingSessions.docs.reduce((obj, doc) => {
-          const {
-            coords,
-            currentQueueID,
-            currentTrackID,
-            owner,
-            id,
-            totals: {listeners: totalListeners, previouslyPlayed: totalPlayed},
-          } = doc.data();
+      // const trendingSessions: FirestoreDocs = await sessionsRef.where('live', '==', true)
+      //   .orderBy('totals.listeners', 'desc')
+      //   .limit(15)
+      //   .get();
 
-          let distance: number = -1;
+      // doSomething(trendingSessions);
 
-          if (coords && pos.coords) {
-            distance = calculateDistance(
-              coords.lat,
-              coords.lon,
-              pos.coords.latitude,
-              pos.coords.longitude,
-            );
-
-            if (distance.toFixed(0) !== 0) {
-              distance = parseInt(distance.toFixed(0));
-            }
-          }
-
-          users = updateObject(users, {
-            [owner.id]: {
-              id: owner.id,
-              displayName: owner.name,
-              profileImage: owner.image,
-              currentSessionID: id,
-            },
-          });
-          
-          return updateObject(obj, {
-            [id]: {
-              id,
+      async function doSomething(trendingSessions, refreshing=false) {
+        if (trendingSessions.empty) {
+          dispatch(updateSessions({ explore: { trendingIDs: [], trendingCanPaginate: true, }, refreshing: true, }));
+          dispatch(actions.success());
+        } else {
+          const trendingIDs: Array<string> = trendingSessions.docs.map(doc => doc.data().id);
+          const trendingCanPaginate: boolean = trendingIDs.length === 15;
+          const tracksToFetch: Array<string> = trendingSessions.docs.map(doc => doc.data().currentTrackID);
+          const trackRes = await Spotify.getTracks(tracksToFetch, {});
+          const music = addMusicItems(trackRes.tracks);
+          // console.log("=============TRENDING!!!")
+          // console.log(trendingIDs)
+          const sessions = trendingSessions.docs.reduce((obj, doc) => {
+            const {
+              coords,
               currentQueueID,
               currentTrackID,
-              distance,
-              totalListeners,
-              totalPlayed,
-              ownerID: owner.id,
-            },
-          });
-        }, {});
+              owner,
+              id,
+              totals: { listeners: totalListeners, previouslyPlayed: totalPlayed },
+            } = doc.data();
 
-        trendingSessions.docs.filter(doc => doc.data().context.type === 'user')
-          .forEach(doc => {
-            const {context: {id, name: displayName}} = doc.data();
-            users = updateObject(users, {[id]: {id, displayName}});
-          });
+            let distance: number = -1;
 
-        trendingSessions.docs.filter(doc => doc.data().context.type === 'playlist')
-          .forEach(doc => {
-            const {context: {id, name}} = doc.data();
-            playlists = updateObject(playlists, {[id]: {id, name, members: [], tracks: []}});
-          });
+            if (coords && pos.coords) {
+              distance = calculateDistance(
+                coords.lat,
+                coords.lon,
+                pos.coords.latitude,
+                pos.coords.longitude,
+              );
 
-        dispatch(addEntities({...music, playlists, sessions, users}));
-        dispatch(updateSessions({explore: {trendingIDs, trendingCanPaginate}}));
-        dispatch(actions.success());
+              if (distance.toFixed(0) !== 0) {
+                distance = parseInt(distance.toFixed(0));
+              }
+            }
+
+            users = updateObject(users, {
+              [owner.id]: {
+                id: owner.id,
+                displayName: owner.name,
+                profileImage: owner.image,
+                currentSessionID: id,
+              },
+            });
+
+            return updateObject(obj, {
+              [id]: {
+                id,
+                currentQueueID,
+                currentTrackID,
+                distance,
+                totalListeners,
+                totalPlayed,
+                ownerID: owner.id,
+              },
+            });
+          }, {});
+
+          trendingSessions.docs.filter(doc => doc.data().context.type === 'user')
+            .forEach(doc => {
+              const { context: { id, name: displayName } } = doc.data();
+              users = updateObject(users, { [id]: { id, displayName } });
+            });
+
+          trendingSessions.docs.filter(doc => doc.data().context.type === 'playlist')
+            .forEach(doc => {
+              const { context: { id, name } } = doc.data();
+              playlists = updateObject(playlists, { [id]: { id, name, members: [], tracks: [] } });
+            });
+
+          dispatch(addEntities({ ...music, playlists, sessions, users }));
+          dispatch(updateSessions({ explore: { trendingIDs, trendingCanPaginate, }, refreshing, }));
+          dispatch(actions.success());
+        }
       }
+
+      // .get();
+
     } catch (err) {
       dispatch(actions.failure(err));
     }
